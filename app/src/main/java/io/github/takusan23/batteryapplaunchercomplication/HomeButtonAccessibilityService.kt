75d83f584322d268bbcbae2d14aa0fcdbc61b5e1
takusan23 <takumu1200@gmail.com>
2022-10-23 19:20:13 +0900

    感触フィードバックが受け取れなくなる問題を回避

--- app/src/main/java/io/github/takusan23/batteryapplaunchercomplication/HomeButtonAccessibilityService.kt
+++ app/src/main/java/io/github/takusan23/batteryapplaunchercomplication/HomeButtonAccessibilityService.kt
@@ -1,10 +1,13 @@
 package io.github.takusan23.batteryapplaunchercomplication
 
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.view.HapticFeedbackConstants
+import android.view.View
 import android.view.accessibility.AccessibilityEvent
 
 /** ホームボタンを押すだけのユーザー補助サービス */
@@ -15,7 +18,23 @@ class HomeButtonAccessibilityService : AccessibilityService() {
         override fun onReceive(context: Context?, intent: Intent?) {
             when (intent?.action) {
                 // ホームボタンを押す
-                DOWN_HOME_BUTTON -> performGlobalAction(GLOBAL_ACTION_HOME)
+                DOWN_HOME_BUTTON -> {
+                    // xml で android:canPerformGestures を指定すると、
+                    // りゅうず を回したときの感触フィードバック が貰えなくなるため、
+                    // 実行時に canPerformGestures を指定する
+                    // ただ、↑のメソッドが隠されているためリフレクションで呼び出す
+                    val setCapabilities = AccessibilityServiceInfo::class.java
+                        .methods
+                        .first { it.name == "setCapabilities" }
+                    setCapabilities.invoke(serviceInfo, AccessibilityServiceInfo.CAPABILITY_CAN_PERFORM_GESTURES)
+                    // 再セットする
+                    serviceInfo = serviceInfo
+                    // ホームボタンを押す
+                    performGlobalAction(GLOBAL_ACTION_HOME)
+                    // そして最後に戻す
+                    setCapabilities.invoke(serviceInfo, 0)
+                    serviceInfo = serviceInfo
+                }
             }
         }
     }
